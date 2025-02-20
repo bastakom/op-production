4e46d2e860ed9b13472e212199bdb4b422ed5fc6
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-02-20 15:18:54 +0100

    fixed about us

--- components/StoryblokProvider.tsx
+++ components/StoryblokProvider.tsx
@@ -3,11 +3,17 @@ import { storyblokInit } from "@storyblok/react/rsc";
 import { PropsWithChildren } from "react";
 import page from "./page";
 import { Hero } from "./hero";
+import { Tiles } from "./tiles";
+import { ImageBlock } from "./image-block";
+import { Gallery } from "./gallery";
 
 storyblokInit({
   components: {
     page: page,
     hero: Hero,
+    tiles: Tiles,
+    image_block: ImageBlock,
+    gallery: Gallery,
   },
 
   enableFallbackComponent: true,
---
+++ components/gallery.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export const Gallery = ({ blok }: any) => {
+  const [visibleImages, setVisibleImages] = useState(4);
+  const [loading, setLoading] = useState(false);
+
+  const handleLoadMore = async () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setVisibleImages((prev) => prev + 4);
+      setLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <div className="py-10">
+      <div className="flex justify-center mt-16 lg:mt-20 mr-auto">
+        <div className="lg:grid grid-cols-4 gap-2 max-w-[100%]">
+          {blok.images.slice(0, visibleImages).map((el: any, index: number) => {
+            return (
+              <div
+                key={index}
+                className="relative !w-[85vw] mb-4 lg:mb-0 lg:!w-[21.8vw] min-h-[50vh] group"
+                style={{
+                  height: "300px",
+                }}
+              >
+                <img
+                  src={el.image.filename}
+                  className="object-cover w-full h-full transition-all duration-300 ease-in-out fill"
+                  alt={el.image.alt}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {visibleImages < blok.images.length && (
+        <div className="flex justify-center mt-6">
+          <button
+            className="buttonRounded"
+            onClick={handleLoadMore}
+            disabled={loading}
+          >
+            {loading ? `Laddar...` : `Ladda fler`}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
