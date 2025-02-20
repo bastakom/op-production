d240240f1e56e8d4a79b032defb51c0d0f6acb24
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-02-20 13:39:09 +0100

    fixed pages

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -5,7 +5,7 @@ type Params = Promise<{ slug: string }>;
 const Page = async ({ params }: { params: Params }) => {
   const pathname = (await params).slug;
   const slugName = pathname === undefined ? `home` : pathname;
-  const story = await getData("home");
+  const story = await getData(slugName);
   return <StoryblokStory story={story} />;
 };
 
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,22 +1,41 @@
 import Image from "next/image";
 import { render } from "storyblok-rich-text-react-renderer";
+import { storyblokEditable } from "@storyblok/react";
 
 export const Hero = ({ blok }: any) => {
   return (
-    <div>
-      <div className="absolute w-full bg-[#0a0a0a] opacity-50 z-10 h-[90%]"></div>
-      <div className="relative w-full h-[90vh]">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="absolute top-0 left-0 w-full object-cover h-[90vh]"
-        >
-          <source src={blok.hero_image.filename} type="video/mp4" />
-        </video>
-      </div>
-      <div className="absolute top-1/2 text-white flex flex-col justify-center items-center gap-4 z-20">
+    <div {...storyblokEditable}>
+      <div
+        className={`absolute w-full bg-[#0a0a0a]  z-10 ${
+          blok.video ? "h-[90%] opacity-50" : "h-[60%] opacity-30"
+        }`}
+      ></div>
+
+      {blok.video ? (
+        <div className="relative w-full h-[90vh]">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute top-0 left-0 w-full object-cover h-[90vh]"
+          >
+            <source src={blok.hero_image.filename} type="video/mp4" />
+          </video>
+        </div>
+      ) : (
+        <div className="relative w-full h-[60vh]">
+          <img
+            src={blok.hero_image.filename}
+            className="object-cover w-full h-full"
+          />
+        </div>
+      )}
+      <div
+        className={`absolute top-1/2 text-white flex flex-col justify-center items-center gap-4 z-20 ${
+          !blok.video && "left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        }`}
+      >
         <h1>{blok.title}</h1>
         <div className="hero-text">{render(blok.content)}</div>
       </div>
