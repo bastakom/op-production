d4c82eee5dfbd10a7c04294feed1159d4f8bb9ac
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-07 10:42:15 +0100

    test

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getAllDestinations } from "@/lib/get-all-destinations";
 import { getData } from "@/lib/get-data";
 import { StoryblokStory } from "@storyblok/react/rsc";
 import { Metadata } from "next";
+//test
 
 type Params = Promise<{ slug: string }>;
 const Page = async ({ params }: { params: Params }) => {
